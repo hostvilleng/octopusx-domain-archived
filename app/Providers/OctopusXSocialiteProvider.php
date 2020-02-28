d7704002613bd162f7950587cb474fb8ebefb5ab
Diyangi <ayotomideaina@gmail.com>
2020-02-28 17:30:22 +0100

    OctopusX Stateless Oauth

--- app/Providers/OctopusXSocialiteProvider.php
+++ app/Providers/OctopusXSocialiteProvider.php
@@ -11,9 +11,77 @@ class OctopusXSocialiteProvider extends AbstractProvider implements ProviderInte
     /**
      * {@inheritdoc}
      */
+
+
     protected function getAuthUrl($state)
     {
-        return $this->buildAuthUrlFromBase('https://accounts.spotify.com/authorize', $state);
+        return $this->buildAuthUrlFromBase('https://auth.octopusx.io', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://auth.octopusx.io/create/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'headers' => ['Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret)],
+            'body'    => $this->getTokenFields($code),
+        ]);
+
+        return $this->parseAccessToken($response->getBody());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_add(
+            parent::getTokenFields($code), 'grant_type', 'authorization_code'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://auth.octopusx.io/v1/me', [
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function formatScopes(array $scopes)
+    {
+        return implode(' ', $scopes);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User)->setRaw($user)->map([
+            'id'       => $user['id'],
+            'nickname' => $user['display_name'],
+            'name'     => $user['display_name'],
+            'avatar'   => !empty($user['images']) ? $user['images'][0]['url'] : null,
+        ]);
     }
 
 
